7086cb7e7d31830c61b3c56a7379bc1a90c99afa
Yuna Portnoy <yunay@yahoo.com>
2014-06-10 13:48:12 -0700

    changed google key

--- app/js/config.js
+++ app/js/config.js
@@ -2,7 +2,7 @@ define([], function() {
   var config = {};
   config.apiKey = '';
   config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
-  config.clientId = '';
+  config.clientId = '244684865093-1ia8d9no6a68ndtce7flb76cbprdut3f.apps.googleusercontent.com';
 
   _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g
